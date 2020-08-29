d188d23dc8d74cbd4881d4835be0c7ebd86b68a4
Arthur <arturkrmnlv10@gmail.com>
2020-08-29 11:27:35 +0600

    Создана модель вопроса, проведена миграция.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=2000, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.question)
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
